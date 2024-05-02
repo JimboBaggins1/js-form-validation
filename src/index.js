50bd3e8bd8facc22bca12d2b3d67872964272cfc
James Anderson <james.anderson98@outlook.com>
2024-05-02 14:34:41 +0100

    Add error handling for email

--- src/index.js
+++ src/index.js
@@ -13,3 +13,42 @@ const countryError = document.querySelector("#country + span.error");
 const codeError = document.querySelector("#post-code + span.error");
 const pwdError = document.querySelector("#pwd + span.error");
 const confPwdError = document.querySelector("#confirmPwd + span.error");
+
+// set event listeners
+window.addEventListener("focusout", (event) => {
+  const target = event.target;
+  switch (target.id) {
+    case "email":
+      console.log(target.id);
+      checkEmailValid();
+      break;
+  }
+});
+
+function checkEmailValid() {
+  if (email.validity.valid) {
+    emailError.textContent = "";
+    emailError.className = "error";
+  } else {
+    showEmailError();
+  }
+}
+
+function showEmailError() {
+  if (email.validity.valueMissing) {
+    // If the field is empty,
+    // display following error message
+    emailError.textContent = 'You need to enter an email address';
+  } else if (email.validity.typeMismatch) {
+    // If entered value isn't an email address
+    // display following error message
+    emailError.textContent = 'Entered value needs to be an email address'
+  } else if (email.validity.tooShort) {
+    // If email is less than required length
+    // display following error message
+    emailError.textContent = `Minimum email length is ${email.minLength} characters, you have entered ${email.value.length}.`
+  }
+
+  // set error class as active
+  emailError.className = 'error active';
+}
