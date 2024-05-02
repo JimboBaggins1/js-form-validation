4ab6ef87e3e297d6cbfb4aeec0b62ea8c9d2c0b4
James Anderson <james.anderson98@outlook.com>
2024-05-02 14:34:20 +0100

    Get form elements

--- src/index.js
+++ src/index.js
@@ -1,9 +1,15 @@
-import './style.css';
-
+import "./style.css";
 
 // get form elements
-const email = document.getElementById('email');
-const country = document.getElementById('country');
-const postCode = document.getElementById('post-code');
-const pwd = document.getElementById('pwd');
-const confPwd = document.getElementById('confirmPwd');
+const email = document.getElementById("email");
+const country = document.getElementById("country");
+const postCode = document.getElementById("post-code");
+const pwd = document.getElementById("pwd");
+const confPwd = document.getElementById("confirmPwd");
+
+// get error elements
+const emailError = document.querySelector("#email + span.error");
+const countryError = document.querySelector("#country + span.error");
+const codeError = document.querySelector("#post-code + span.error");
+const pwdError = document.querySelector("#pwd + span.error");
+const confPwdError = document.querySelector("#confirmPwd + span.error");
