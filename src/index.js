34357206c523d590b7937480dde066c94be1a1c8
James Anderson <james.anderson98@outlook.com>
2024-05-02 13:54:12 +0100

    Import style. Get form elements

---
+++ src/index.js
@@ -0,0 +1,9 @@
+import './style.css';
+
+
+// get form elements
+const email = document.getElementById('email');
+const country = document.getElementById('country');
+const postCode = document.getElementById('post-code');
+const pwd = document.getElementById('pwd');
+const confPwd = document.getElementById('confirmPwd');
