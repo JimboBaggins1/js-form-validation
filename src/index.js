dc9516a6012da0e03979366bc07305c1e9b722f4
James Anderson <james.anderson98@outlook.com>
2024-05-06 14:57:45 +0100

    Start making check/error functions more generic

--- src/index.js
+++ src/index.js
@@ -8,47 +8,92 @@ const pwd = document.getElementById("pwd");
 const confPwd = document.getElementById("confirmPwd");
 
 // get error elements
-const emailError = document.querySelector("#email + span.error");
-const countryError = document.querySelector("#country + span.error");
-const codeError = document.querySelector("#post-code + span.error");
-const pwdError = document.querySelector("#pwd + span.error");
-const confPwdError = document.querySelector("#confirmPwd + span.error");
+// const emailError = document.querySelector("#email + span.error");
+// const countryError = document.querySelector("#country + span.error");
+// const codeError = document.querySelector("#post-code + span.error");
+// const pwdError = document.querySelector("#pwd + span.error");
+// const confPwdError = document.querySelector("#confirmPwd + span.error");
 
 // set event listeners
 window.addEventListener("focusout", (event) => {
   const target = event.target;
   switch (target.id) {
     case "email":
-      console.log(target.id);
-      checkEmailValid();
+      checkValid(target.id);
       break;
+    case "country":
+      checkValid(target.id);
+      break;
+    case "post-code":
+      checkValid(target.id);
+      break;
+    case "pwd":
+      checkValid(target.id);
+      break;
+    case "confirmPwd":
+      checkValid(target.id);
+      break;
+    default:
+      break;  
   }
 });
 
-function checkEmailValid() {
-  if (email.validity.valid) {
-    emailError.textContent = "";
-    emailError.className = "error";
+//change to generic check valid func
+function checkValid(identifier) {
+  const formElem = document.getElementById(identifier);
+  const formError = document.querySelector(`#${identifier} + span.error`);
+  if (formElem.validity.valid) {
+    formError.textContent = "";
+    formError.className = "error";
   } else {
-    showEmailError();
+    showError(identifier);
   }
 }
 
-function showEmailError() {
-  if (email.validity.valueMissing) {
-    // If the field is empty,
-    // display following error message
-    emailError.textContent = 'You need to enter an email address';
-  } else if (email.validity.typeMismatch) {
+// function checkEmailValid() {
+//   if (email.validity.valid) {
+//     emailError.textContent = "";
+//     emailError.className = "error";
+//   } else {
+//     showEmailError();
+//   }
+// }
+
+function showError(errorType) {
+  const formElem = document.getElementById(errorType);
+  const errorElem = document.querySelector(`#${errorType} + span.error`);
+  console.log(typeof errorType.slice(0, 1));
+  console.log(errorElem);
+  if (formElem.validity.valueMissing) {
+    errorElem.textContent = `You need to enter ${errorType.slice(0, 1).match(/[aeiouAEIOU]/) ? 'an' : 'a'} ${errorType}.`
+  } else if (formElem.validity.typeMismatch) {
     // If entered value isn't an email address
     // display following error message
-    emailError.textContent = 'Entered value needs to be an email address'
-  } else if (email.validity.tooShort) {
+    errorElem.textContent = `Entered value needs to be ${errorType.slice(0, 1).match(/[aeiouAEIOU]/) ? 'an' : 'a'} ${errorType}.`
+  } else if (formElem.validity.tooShort) {
     // If email is less than required length
     // display following error message
-    emailError.textContent = `Minimum email length is ${email.minLength} characters, you have entered ${email.value.length}.`
+    errorElem.textContent = `Minimum ${errorType} length is ${formElem.minLength} characters, you have entered ${formElem.value.length}.`
   }
 
-  // set error class as active
-  emailError.className = 'error active';
+  errorElem.className = 'error active';
 }
+
+// function showEmailError() {
+//   if (email.validity.valueMissing) {
+//     // If the field is empty,
+//     // display following error message
+//     emailError.textContent = 'You need to enter an email address';
+//   } else if (email.validity.typeMismatch) {
+//     // If entered value isn't an email address
+//     // display following error message
+//     emailError.textContent = 'Entered value needs to be an email address'
+//   } else if (email.validity.tooShort) {
+//     // If email is less than required length
+//     // display following error message
+//     emailError.textContent = `Minimum email length is ${email.minLength} characters, you have entered ${email.value.length}.`
+//   }
+
+//   // set error class as active
+//   emailError.className = 'error active';
+// }
